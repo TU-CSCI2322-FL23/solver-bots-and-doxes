594b905c2416ec6aec674a034cc1873772656c44
Noah Wenig <nwenig@trinity.edu>
2023-11-10 11:12:58 -0600

    Base case for whoWillWin

--- Boxes.hs
+++ Boxes.hs
@@ -15,13 +15,7 @@ type Game = (Board, Boxes, Player, Int)
 --DONT LET THEM MAKE A GAME OF SIZE 1 IT WILL NEVER END
 
 --allPossibleEdges =[((x, y), dir) | x <- [1..size-1], y <- [1..size-1], dir <- [Rgt, Dwn]] 
-allPossibleEdges :: Int -> [Edge]
-allPossibleEdges size = rgts ++ dwns 
-  where  rgts = [((x,y), Rgt) | x <- [1..size-1], y <- [1..size]]
-         dwns = [((x,y), Dwn) | x <- [1..size], y<-[1..size-1]]
 
-validMoves :: Game -> [Edge]
-validMoves (board,_,_,size) = allPossibleEdges size \\ board
 
 opponent:: Player -> Player
 opponent P1 = P2
@@ -37,7 +31,7 @@ isInBounds game@(_, _, _, size) ((x, y), Rgt) = (x<size) && (x>=1) && (y<=size) 
 --assuming game inputed is a finished game
 findWinner :: Game -> Maybe Outcome
 findWinner (board, boxes, _, size)
-  | p1_score < ((size-1) * (size-1)`div`2) && p2_score < ((size-1) * (size-1)`div`2) = Nothing
+  | p1_score +p2_score< ((size-1) *(size-1)) = Nothing
   | p1_score == p2_score = Just Tie 
   | p1_score > p2_score = Just (Players P1) 
   | otherwise = Just (Players P2)
@@ -60,27 +54,37 @@ checkBox (x, y) edge_list = and [edge `elem` edge_list | edge <- boxEdges]
     where boxEdges = [((x, y), Rgt), ((x,y),Dwn), ((x+1,y),Dwn) , ((x,y+1),Rgt)]
 --Build a row of edges starting at a given point
 
---Takes a Game state and an edge (move) and returns the new game state. 
-makeMove :: Game -> Edge -> Game
+--not tested :(
+makeMove :: Game -> Edge -> Maybe Game
 makeMove game@(board, boxes, player, int) move
-  | not (validMove move board) = error "Invalid Move, it already exists." --if not a valid more, throw error 
-  | not (isInBounds game move) = error "Invalid Move, out of Bounds"
-  | null newBoxes = (move:board, boxes, opponent player, int) --if list of new boxes is empty, return new game w/move added to board, same boxes, switch player
-  | otherwise = (move:board, newBoxes++boxes, player, int) --if new boxes, return new game w/move added to board, the nex boxes made added to existing boxes, same player
+  | not (validMove move board) ||not (isInBounds game move) = Nothing 
+  | null newBoxes = Just (move:board, boxes, opponent player, int) --if list of new boxes is empty, return new game w/move added to board, same boxes, switch player
+  | otherwise = Just (move:board, newBoxes++boxes, player, int) --if new boxes, return new game w/move added to board, the nex boxes made added to existing boxes, same player
   where
       newBoxes = makeBoxes move game
 
---Alternative version that returns the same game if invalid move, message can be dealt with in IO
---ex. if same game returned, print message saying an invalid move was entered 
---not tested :(
---makeMove :: Game -> Edge -> Game
---makeMove game@(board, boxes, player, int) move
---  | not (validMove move board) ||not (isInBounds game move) = (board, boxes, player, int) --would return the same game(wihtout the invalid move)
---  | not (isInBounds game move) = error "Invalid Move, out of Bounds"
---  | null newBoxes = (move:board, boxes, opponent player, int) --if list of new boxes is empty, return new game w/move added to board, same boxes, switch player
---  | otherwise = (move:board, newBoxes++boxes, player, int) --if new boxes, return new game w/move added to board, the nex boxes made added to existing boxes, same player
---  where
---      newBoxes = makeBoxes move game
+whoWillWin :: Game -> Outcome
+whoWillWin game = case winner of Just Outcome guy -> guy
+                                  Nothing nothing -> pickOutcome (map whoWillWin lst)
+                     where winner = findWinner game 
+                            moves = allPossibleEdges
+                            lst = [makeMove x game | x <- moves]
+
+
+pickOutcome :: [Outcome] -> Outcome
+pickOutcome  lst = undefined 
+                          
+
+allPossibleEdges :: Int -> [Edge]
+allPossibleEdges size = rgts ++ dwns 
+  where  rgts = [((x,y), Rgt) | x <- [1..size-1], y <- [1..size]]
+         dwns = [((x,y), Dwn) | x <- [1..size], y<-[1..size-1]]
+
+validMoves :: Game -> [Edge]
+validMoves (board,_,_,size) = allPossibleEdges size \\ board
+
+
+
 
 
 --PRETTY PRINT SECTION
@@ -169,3 +173,14 @@ prettyPrint game = do
         let strings = prettyShow game
         mapM_ putStrLn strings
       
+
+      --OLD MAKE MOVE PURGATORY 
+--Takes a Game state and an edge (move) and returns the new game state. 
+--makeMove :: Game -> Edge -> Game
+--makeMove game@(board, boxes, player, int) move
+--  | not (validMove move board) = error "Invalid Move, it already exists." --if not a valid more, throw error 
+--  | not (isInBounds game move) = error "Invalid Move, out of Bounds"
+--  | null newBoxes = (move:board, boxes, opponent player, int) --if list of new boxes is empty, return new game w/move added to board, same boxes, switch player
+--  | otherwise = (move:board, newBoxes++boxes, player, int) --if new boxes, return new game w/move added to board, the nex boxes made added to existing boxes, same player
+--  where
+--      newBoxes = makeBoxes move game
