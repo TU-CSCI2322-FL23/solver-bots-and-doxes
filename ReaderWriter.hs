f0554a89af59b5c4b6c094511ae0ba7838e44c94
nflores <nflores2@trinity.edu>
2023-11-10 00:41:50 -0600

    added comments

--- ReaderWriter.hs
+++ ReaderWriter.hs
@@ -38,7 +38,7 @@ readDirection "D" = Dwn
 
 
 
-showGame :: Game -> String
+showGame :: Game -> String --takes a game and converts the game state into a string using the unlines function.
 showGame (board, boxes, player, size) =
     unlines [unwords $ map showEdge board, unwords $ map showBox boxes, showPlayer player, show size]
 
@@ -49,7 +49,7 @@ showEdge ((x, y), dir) = unwords [show x, show y, showDirection dir]
 showBox :: Box -> String
 showBox ((x, y), player) = unwords [show x, show y, showPlayer player]
 
-showPlayer :: Player -> String
+showPlayer :: Player -> String -- I now realise we have a getPlayer
 showPlayer P1 = "P1"
 showPlayer P2 = "P2"
 
@@ -59,11 +59,11 @@ showDirection Dwn = "D"
 
 
 -- IO action to write a game state to a file
-writeGame :: Game -> FilePath -> IO ()
+writeGame :: Game -> FilePath -> IO () -- takes a game than converts it into a file, writeFile: An IO action that writes the content to a file. filePath: The file path where the game state will be stored.
 writeGame game filePath = writeFile filePath (showGame game)
 
 -- IO action to load a game state from a file
-loadGame :: FilePath -> IO Game
+loadGame :: FilePath -> IO Game -- readFile: An IO action that reads the content of a file. readGame: Converts the string content from the file into a game state
 loadGame filePath = do
     content <- readFile filePath
     return $ readGame content
@@ -71,7 +71,7 @@ loadGame filePath = do
 --loadedGame <- loadGame "testGame.txt"        writeGame sampleGame "testGame.txt"          let sampleGame = ([], [], P1, 3) 
 {-
 -- IO action to compute and print the best move along with the outcome it forces. Note: this is very untested the cmd should be "putBestMove loadedGame" with 
-putBestMove :: Game -> IO ()
+putBestMove :: Game -> IO () -- If you have a game state currentGame and you want to find and print the best move, you would call putBestMove currentGame.
 putBestMove game = do
     let move = bestMove game
         outcome = findWinner (makeMove game move)
