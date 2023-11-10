dcbb885ca1a4f8ad7f851206b9cfca0e57845291
2022-08-22 <atollett@janus02.cs.trinity.edu>
2023-11-10 12:20:47 -0600

    completed whoWillWin, appears to pass all current tests

--- Boxes.hs
+++ Boxes.hs
@@ -63,30 +63,6 @@ makeMove game@(board, boxes, player, int) move
   where
       newBoxes = makeBoxes move game
 
-whoWillWin :: Game -> Outcome
-whoWillWin game = case winner of Just Outcome guy -> guy
-                                  Nothing nothing -> pickOutcome (map whoWillWin lst)
-                     where winner = findWinner game 
-                            moves = allPossibleEdges
-                            lst = [makeMove x game | x <- moves]
-
-
-pickOutcome :: [Outcome] -> Outcome
-pickOutcome  lst = undefined 
-                          
-
-allPossibleEdges :: Int -> [Edge]
-allPossibleEdges size = rgts ++ dwns 
-  where  rgts = [((x,y), Rgt) | x <- [1..size-1], y <- [1..size]]
-         dwns = [((x,y), Dwn) | x <- [1..size], y<-[1..size-1]]
-
-validMoves :: Game -> [Edge]
-validMoves (board,_,_,size) = allPossibleEdges size \\ board
-
-
-
-
-
 --PRETTY PRINT SECTION
 combineRows :: [[(Point, String)]] -> [String]
 combineRows rows = map concatRow rows
--- Solver.hs
+++ Solver.hs
@@ -1,15 +1,22 @@
 import Boxes 
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Maybe
+import Data.List
 
 whoWillWin :: Game -> Outcome
-whoWillWin game = case winner of Just Outcome guy -> guy
-                                  Nothing nothing -> pickOutcome (map whoWillWin lst)
-                     where winner = findWinner game 
-                            moves = allPossibleEdges
-                            lst = [makeMove x game | x <- moves]
+whoWillWin game@(_,_,player,size) = case (findWinner game) of 
+                            Just guy -> guy
+                            Nothing -> pickOutcome player (map whoWillWin lst)
+                                   where  moves = allPossibleEdges size
+                                          lst = catMaybes [makeMove game x | x <- moves]
 
 
-pickOutcome :: [Outcome] -> Outcome
-pickOutcome  lst = undefined 
+pickOutcome :: Player -> [Outcome] -> Outcome
+pickOutcome player lst 
+       | Players player `elem` lst = (Players player)
+       | Tie `elem` lst = Tie
+       | otherwise = Players (opponent player)
 
 
 allPossibleEdges :: Int -> [Edge]
