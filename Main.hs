e4897111e183297466b9d3afa5d490f07ff10bb1
2022-08-22 <atollett@janus02.cs.trinity.edu>
2023-11-27 12:20:05 -0600

    started implementing command line flag stuff

--- Main.hs
+++ Main.hs
@@ -5,12 +5,26 @@ import Data.List
 import Data.List.Split
 import Solver
 import Data.Maybe
+import Text.Read (readMaybe)
 import System.Environment
+import System.Console.GetOpt
 --import Debug.Trace
 --import Text.Read
 --import GHC.Generics (R, D)
 --import Distribution.Compat.Lens (_1)
 --import System.Directory
+
+
+data Flag = Help | Winner | Depth String | Move String | Verbose | Interactive | deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options = [ Option ['h'] ["help"] (NoArg Help) "Print usage information and exit."
+          , Option ['w'] ["winner"] (NoArg Winner ) "Print best move using exhaustive search."
+          , Option ['d'] ["depth"] (ReqArg Depth "<dep>") "Print best move using bounded search of depth <dep>."
+          , Option ['m'] ["move"] (ReqArg Mpve "<move>") "Print resulting board of making move <move>"
+          , Option ['v'] ["verbose"] (NoArg Verbose) "Prints bestMove and how good it is (win, tie, loss, rating, etc...)"
+          ]
+
 
 main :: IO ()
 main = do
