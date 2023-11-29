5f947aebc9b93da618d7e76c20a560da7b722a04
Daisy Nieto <daisynieto@Daisys-MacBook-Air.local>
2023-11-29 11:43:28 -0600

    adjusted rateGame

--- Solver.hs
+++ Solver.hs
@@ -45,20 +45,58 @@ rateGame :: Game -> Int
 rateGame game@(board, boxes, player, size)
     | isGameOver game = evaluateOutcome (findWinner game) player
     | otherwise = evaluatePosition game player
-
+{-
 evaluateOutcome :: Maybe Outcome -> Player -> Int
 evaluateOutcome (Just (Players p)) currentPlayer
     | p == currentPlayer = 100  -- Winning the game is highly favorable
     | otherwise = -100  -- Losing the game is highly unfavorable
 evaluateOutcome (Just Tie) _ = 0  -- A tie is neutral
-evaluateOutcome Nothing _ = 0  -- Game is ongoing, no outcome yet
+--evaluateOutcome Nothing _ = 0  -- Game is ongoing, no outcome yet
 --so if P1 is favorable than -100 else P2 for 100
 evaluatePosition :: Game -> Player -> Int
-evaluatePosition (board, boxes, player, size) currentPlayer =
-    currentPlayerBoxes - opponentBoxes
+evaluatePosition (board, boxes, player, size) currentPlayer 
+    |(currentPlayerBoxes + boxesLeft > opponentBoxes) = (currentPlayerBoxes+boxesLeft)*fracConversion
+    |(opponentBoxes + boxesLeft > currentPlayerBoxes) =(opponentBoxes+boxesLeft)*fracConversion*(-1)
+    |otherwise = 0
   where
     currentPlayerBoxes = length $ filter (\(_, p) -> p == currentPlayer) boxes
     opponentBoxes = length $ filter (\(_, p) -> p == opponent currentPlayer) boxes
+    totalBoxes = (size-1)*(size-1)
+    fracConversion = round (100/fromIntegral totalBoxes)
+    boxesLeft = totalBoxes - (length boxes)
+-}
+---------new one-------------cant use current player and opponent
+evaluateOutcome :: Maybe Outcome -> Player -> Int
+evaluateOutcome (Just (Players p)) currentPlayer
+    | p == P1 = 100  -- Winning the game is highly favorable
+    | otherwise = -100  -- Losing the game is highly unfavorable
+evaluateOutcome (Just Tie) _ = 0  -- A tie is neutral
+--evaluateOutcome Nothing _ = 0  -- Game is ongoing, no outcome yet
+--so if P1 is favorable than -100 else P2 for 100
+evaluatePosition :: Game -> Player -> Int
+evaluatePosition (board, boxes, player, size) currentPlayer 
+    |(player1Boxes + boxesLeft > player2Boxes)&& (currentPlayer == P1) = (player1Boxes+boxesLeft)*fracConversion
+    |(player2Boxes + boxesLeft > player1Boxes) && (currentPlayer == P2) =(player2Boxes+boxesLeft)*fracConversion*(-1)
+    |(player1Boxes + boxesLeft > player2Boxes) && (currentPlayer == P2) =(player2Boxes+boxesLeft)*fracConversion*(-1)
+    |(player2Boxes + boxesLeft > player1Boxes) && (currentPlayer == P1) =(player1Boxes+boxesLeft)*fracConversion
+    |otherwise = 0
+  where
+    player1Boxes = length $ filter (\(_, p) -> p == P1) boxes
+    player2Boxes = length $ filter (\(_, p) -> p == P2) boxes
+    totalBoxes = (size-1)*(size-1)
+    fracConversion = round (100/fromIntegral totalBoxes)
+    boxesLeft = totalBoxes - (length boxes)
+
+{-game5 = (
+[((1,1),Rgt),((1,1),Dwn),((1,2),Dwn),((3,1),Dwn),((2,2),Rgt),((1,3),Rgt),((2,3),Rgt),((2,1),Dwn),((2,1),Rgt),((2,2),Dwn),((3,2),Dwn)] , [((2,1), P1), ((2,2), P2)],P2,3 ) 
+game6 = ( [((1,1),Rgt),((1,1),Dwn),((1,2),Dwn),((3,1),Dwn),((2,2),Rgt),((1,3),Rgt),((2,3),Rgt),((2,1),Dwn),((2,1),Rgt ),((2,2),Dwn),((3,2),Dwn)] , [((2,1), P1), ((2,2), P2)],P1,3 )
+rateGame game5 -- output : -75 // player2 turn
+rateGame game6 -- output : 75 // player1 turn
+game1 = ([((1,1),Rgt),((1,1),Dwn),((2,1),Dwn),((1,2),Rgt)],[((1, 1), P1)], P2, 3)
+game2 = ([((1,1),Rgt),((1,1),Dwn),((2,1),Dwn),((1,2),Rgt)],[((1, 1), P1)], P1, 3)
+rateGame game1 -- output : -75
+rateGame game2 -- output : 100
+-}
 --Note this uses isGameOver from boxes.hs
 
 --}
@@ -103,6 +141,6 @@ minimize depth game
 --let sampleGame = ([((1,1),Rgt),((1,1),Dwn),((1,2),Dwn),((3,1),Dwn),((2,2),Rgt),((1,3),Rgt),((2,3),Rgt),((2,1),Dwn),((2,1),Rgt)],[((2,1), P1)],P1,3)
 
 --Note: this uses fromMaybe which fogarty might not like
-
+--game3 =  ( [((1,1),Rgt),((1,1),Dwn),((1,2),Dwn),((3,1),Dwn),((2,2),Rgt),((1,3),Rgt),((2,3),Rgt),((2,1),Dwn),((2,1),Rgt ),((2,2),Dwn),((3,2),Dwn)] , [((2,1), P1), ((2,2), P2)],P1,3 )
 
 
